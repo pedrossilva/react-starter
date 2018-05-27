90de7310f67fccc13e76034de63743599793641c
Pedro Silva <pedrossilva.br@gmail.com>
2018-05-27 10:31:42 -0300

    Initial commit

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import s from './App.css'
+
+class App extends Component {
+
+    render() {
+        return <h1 className={s.fontTitle}>Hello, React!</h1>
+    }
+
+}
+
+export default App
